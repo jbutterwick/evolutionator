41f496445664e0340b5325e15d570f48ebe9fc1f
Jordan Butterwick <jordan@butterwick.tech>
2022-07-05 22:04:12 -0600

    init

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Position { x: f32, y: f32 }
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn print_position_system(query: Query<&Transform>) {
+    for transform in query.iter() {
+        println!("position: {:?}", transform.translation);
+    }
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn().insert(Person).insert(Name("Elaina Proctor".to_string()));
+    commands.spawn().insert(Person).insert(Name("Renzo Hume".to_string()));
+    commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
+
+struct Entity(u64);
+
+fn hello_world() {
+    println!("hello evolutionator!!");
+}
+
+fn main() {
+    App::new()
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .add_system(greet_people)
+        .run();
+}
