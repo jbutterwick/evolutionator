56029c27fa3e5b121778a82d6935630493695e66
jordanbutterwick <jordan.butterwick@statflo.com>
2022-10-03 15:41:00 -0600

    fixing problems

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
 use bevy::prelude::*;
-use rand::{Rng, SeedableRng};
+use rand::Rng;
 use std::cmp::{max, min};
+
+const TEXT_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
+
+// Enum that will be used as a global state for the game
+#[derive(Clone, Eq, PartialEq, Debug, Hash)]
+enum GameState {
+    Splash,
+    Menu,
+    Game,
+}
+
+#[derive(Resource, Debug, Component, PartialEq, Eq, Clone, Copy)]
+struct Volume(u32);
+
+// One of the two settings that can be set through the menu. It will be a resource in the app
+#[derive(Resource, Debug, Component, PartialEq, Eq, Clone, Copy)]
+enum DisplayQuality {
+    Low,
+    Medium,
+    High,
+}
 
 #[derive(Component)]
 struct Position {
@@ -10,6 +31,7 @@ struct Position {
 
 #[derive(Component, Clone)]
 struct Organism {
+    id: i32,
     dna: [u8; 10],
 }
 
@@ -22,15 +44,12 @@ fn print_position_system(query: Query<&Transform>) {
 }
 
 fn add_organism(mut commands: Commands) {
-    commands.spawn().insert(Organism {
-        dna: rand::random::<[u8; 10]>(),
-    });
-    commands.spawn().insert(Organism {
-        dna: rand::random::<[u8; 10]>(),
-    });
-    commands.spawn().insert(Organism {
-        dna: rand::random::<[u8; 10]>(),
-    });
+    for id in num {
+        commands.spawn().insert(Organism {
+            id,
+            dna: rand::random::<[u8; 10]>(),
+        });
+    }
 }
 
 struct GreetTimer(Timer);
@@ -39,35 +58,47 @@ fn randomly_generate_dna() -> [u8; 10] {
     rand::random::<[u8; 10]>()
 }
 
-fn greet_organism(
-    time: Res<Time>,
-    mut timer: ResMut<GreetTimer>,
-    query: Query<&Name, With<Organism>>,
-) {
+fn greet_organism(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Organism>) {
     if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("hello {}!", name.0);
+        for organism in query.iter() {
+            println!("hello {}!", organism.id);
         }
     }
 }
-// oh sexy
-fn sexual_reproduction(mother: &Organism, father: &Organism) -> Organism {
-    let dna = mother
-        .dna
-        .iter()
-        .flat_map(|gene1| {
-            father.dna.iter().flat_map(|gene2| {
-                let mut rng = rand::thread_rng();
-                rng.gen_range(min(gene1, gene2)..max(gene1, gene2))
-            })
-        })
-        .collect();
 
-    Organism { dna }
+fn get_bit_at(input: u8, n: u8) -> Option<bool> {
+    if n < 8 {
+        Some(input & (1 << n) != 0)
+    } else {
+        None
+    }
 }
 
-fn asexual_reproduction(organism: &Organism) -> Organism {
-    organism.clone()
+// oh sexy
+fn sexual_reproduction(mut commands: Commands, mother: &Organism, father: &Organism) {
+    if mother.dna.multiplicative && father.dna.multiplicative {}
+    commands.spawn().insert(Organism {
+        id,
+        dna: mother
+            .dna
+            .into_iter()
+            .flat_map(|gene1| {
+                father.dna.into_iter().flat_map(|gene2| {
+                    let mut rng = rand::thread_rng();
+                    rng.gen_range(min(gene1, gene2)..max(gene1, gene2))
+                })
+            })
+            .collect(),
+    });
+}
+
+fn asexual_reproduction(mut commands: Commands, organism: &Organism, num: i32) {
+    for id in num {
+        commands.spawn().insert(Organism {
+            id,
+            dna: organism.dna.clone(),
+        });
+    }
 }
 
 fn hello_world() {
